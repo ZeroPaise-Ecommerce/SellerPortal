a84ad2386d9ca88833dc85edee3617514e95fc08
Asen <80930424+AsenBashaC@users.noreply.github.com>
2025-07-20 13:01:54 +0530

    Expenses changes

--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -2,9 +2,13 @@ import { combineReducers } from '@reduxjs/toolkit';
 //import productReducer from '../features/product/productSlice';
 import { productReducer } from './Inventory/product/reducer';
 import { supplierReducer } from './Inventory/supplier/reducer';
+import { PurchaseOrderReducer } from './Inventory/purchase/reducer';
+import { expenseReducer } from './Inventory/purchase/reducer';
 const rootReducer = combineReducers({
   product: productReducer,
-  supplier: supplierReducer, // Assuming you have a supplierReducer
+  supplier: supplierReducer,
+  purchase: PurchaseOrderReducer,
+  expense: expenseReducer
   // Add other reducers here
 });
 
